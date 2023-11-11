0bdf85c8fa0b305b99b279f359a49da066c7ddb1
Carlos Menke <carlos.menke@icloud.com>
2023-11-11 16:14:09 +0000

    parsing the first line. TODO: Switch state hirarchy for the groups in the header (First Line, Varaibles, Body)

--- webserv/inc/RequestParser.hpp
+++ webserv/inc/RequestParser.hpp
@@ -8,32 +8,41 @@ enum ParseState {
     METHOD,
     REQUEST_URI,
     HTTP_VERSION,
-    HEADER_FIELD,
-    HEADER_VALUE,
-    REQUEST_BODY,
+    // HEADER_FIELD,
+    // HEADER_VALUE,
+    // REQUEST_BODY,
+    ERROR,
     DONE
 };
 
-
-
+enum HttpMethod {
+    GET,
+    POST,
+    DELETE,
+    UNKNOWN
+};
 
 class HttpRequestParser {
 public:
 
-	void parseToken(ParseState state, const std::string& input, size_t& pos, std::string& currentToken);
+    void parseToken(int& state, const std::string& input);
     void parse(const std::string& request);
 
 private:
 
 	// member variables for storing parsed HTTP request
 	struct HttpRequestData {
-        HttpMethod method;
+        int         method;
         std::string requestUri;
         std::string httpVersion;
         std::map<std::string, std::string> headers;
         std::string requestBody;
 		
     };
+
+    std::string getToken(const std::string& request, size_t& pos);
+    int parseMethod(const std::string& methodStr);
+    int nextState(int currentState);
 
 	HttpRequestData storedHttpRequest;
 
