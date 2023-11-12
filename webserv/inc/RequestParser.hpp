088f494ef7e917abfa8eceb74b23a3f286eca6be
Carlos Menke <carlos.menke@icloud.com>
2023-11-12 05:17:02 +0000

    cleaned the request parser. Added error checking for the first line. TODO: Define rules for header fields and body

--- webserv/inc/RequestParser.hpp
+++ webserv/inc/RequestParser.hpp
@@ -1,50 +1,69 @@
-#pragma once
+#ifndef REQUEST_PARSER_NEW_HPP
+# define REQUEST_PARSER_NEW_HPP
 
-#include <iostream>
-#include <string>
-#include "Server.hpp"
+# include "Server.hpp"
 
-enum ParseState {
-    METHOD,
-    REQUEST_URI,
-    HTTP_VERSION,
-    // HEADER_FIELD,
-    // HEADER_VALUE,
-    // REQUEST_BODY,
-    ERROR,
-    DONE
+enum ParseState
+{
+	REQUEST_LINE,
+	// METHOD,
+	// REQUEST_URI,
+	// HTTP_VERSION,
+	HEADER_FIELDS,
+	// HEADER_NAME,
+	// HEADER_VALUE,
+	REQUEST_BODY,
+	ERROR, // Bad request
+	SUCCESS,
+	DONE
 };
 
-enum HttpMethod {
-    GET,
-    POST,
-    DELETE,
-    UNKNOWN
+enum HttpMethod
+{
+	GET,
+	POST,
+	DELETE,
+	UNKNOWN
 };
 
-class HttpRequestParser {
-public:
-
-    void parseToken(int& state, const std::string& input);
-    void parse(const std::string& request);
-
-private:
-
-	// member variables for storing parsed HTTP request
-	struct HttpRequestData {
-        int         method;
-        std::string requestUri;
-        std::string httpVersion;
-        std::map<std::string, std::string> headers;
-        std::string requestBody;
-		
-    };
-
-    std::string getToken(const std::string& request, size_t& pos);
-    int parseMethod(const std::string& methodStr);
-    int nextState(int currentState);
-
-	HttpRequestData storedHttpRequest;
-
-
+enum ErrorCodes
+{
+	OK,
+	BAD_REQUEST,
+	NOT_IMPLEMENTED
 };
+
+class RequestParserNew
+{
+	public:
+		RequestParserNew(void);
+		RequestParserNew(const RequestParserNew &src);
+		~RequestParserNew(void);
+		RequestParserNew &operator=(const RequestParserNew &rhs);
+
+		void parse(const std::string& request); // needs to be looped
+
+		struct HttpRequestData
+		{
+			int			method;
+			std::string requestUri;
+			std::map<std::string, std::string> headerFields;
+			std::string requestBody;
+    	};
+		HttpRequestData storedHttpRequest;
+
+	private:
+		int						_currentStateMachine;
+		int						_errorCode;
+		std::string::size_type	_currentPositionInInput;
+		std::string				_parameter;
+		std::string				_value;
+
+		int	_parseRequestLine(const std::string& input);
+		int _setMethod(const std::string& input);
+		int _parseHeaderFields(const std::string& input);
+		int	_setHeaderPair(const std::string& name, const std::string& value);
+		int	_parseRequestBody(const std::string& input);
+};
+
+#endif /* REQUEST_PARSER_NEW_HPP */
