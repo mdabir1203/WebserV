ca5ed8c147d26d5b0c150558d495b966081c2e14
Carlos Menke <carlos.menke@icloud.com>
2023-11-15 20:52:56 +0000

    added logic to parse one header line. edited the tests to new layout

--- webserv/inc/HeaderField.hpp
+++ webserv/inc/HeaderField.hpp
@@ -1,6 +1,9 @@
 #ifndef HEADER_FIELD_HPP
 # define HEADER_FIELD_HPP
 
+#include "Server.hpp"
+#include "RequestParser.hpp"
+
 #include <map>
 #include <string>
 #include <cctype>
@@ -19,9 +22,12 @@ class HeaderFieldStateMachine {
 public:
    HeaderFieldStateMachine(void);
 
+   int   parseOneHeaderLine(const std::string& input);
    void  parseChar(char input);
    const std::map<std::string, std::string>& getParsedHeaders() const;
    void  setCurrentState(const int state);
+   void  setInputPosition(const size_t position);
+   void  reset(void);
 
 private:
    const size_t                         maxHeaderLength;  // To prevent buffer overflow attacks
@@ -29,6 +35,7 @@ private:
    std::string                          headerName;
    std::string                          headerValue;
    std::map<std::string, std::string>   headers;
+   size_t                               positionInInput;
 };
 
 #endif /* HEADER_FIELD_HPP */
--- webserv/src/server/testParse.cpp
+++ webserv/src/server/testParse.cpp
@@ -13,10 +13,8 @@
 void testValidHeader() {
     HeaderFieldStateMachine parser;
     std::string header = "Content-Type: text/html\r\n";
-    
-    for (size_t i = 0; i < header.length(); ++i) {
-        parser.parseChar(header[i]);
-    }
+
+    parser.parseOneHeaderLine(header);
 
     assert(parser.getParsedHeaders().at("content-type") == "text/html");
     std::cout << "Test Valid Header: Passed\n";
@@ -27,13 +25,8 @@ void testInvalidHeaderName() {
     HeaderFieldStateMachine parser;
     std::string header = "Content@Type: text/html\r\n";
     
-    try {
-        for (size_t i = 0; i < header.length(); ++i) {
-            parser.parseChar(header[i]);
-        }
-    } catch (const std::runtime_error& e) {
+    if (parser.parseOneHeaderLine(header) != OK)
         caught = true;
-    }
 
     assert(caught);
     std::cout << "Test Invalid Header Name: Passed\n";
@@ -43,9 +36,7 @@ void testLeadingAndTrailingWhitespace() {
     HeaderFieldStateMachine parser;
     std::string header = "Host:    example.com    \r\n";
     
-    for (size_t i = 0; i < header.length(); ++i) {
-        parser.parseChar(header[i]);
-    }
+    parser.parseOneHeaderLine(header);
 
     assert(parser.getParsedHeaders().at("host") == "example.com");
     std::cout << "Test Leading and Trailing Whitespace: Passed\n";
@@ -58,21 +49,14 @@ void testHeadersWithMultipleValues() {
 
     std::string headers2= "Set-Cookie: id=12345; Expires=Thu, 22 Oct 2015 07:28:00 GMT\r\n";
     
-    for (size_t i = 0; i < headers1.length(); ++i) {
-        std::cout << headers1[i] << std::flush ;
-        parser.parseChar(headers1[i]);
-        
-    }
+    parser.parseOneHeaderLine(headers1);
 
     assert(parser.getParsedHeaders().at("set-cookie") == "id=a3fWa; Expires=Wed, 21 Oct 2015 07:28:00 GMT");
     std::cout << "Test Headers with Multiple Values: Passed\n";
 
     parser.setCurrentState(HEADER_NAME);
-    for (size_t i = 0; i < headers2.length(); ++i) {
-    std::cout << headers2[i] << std::flush ;
-    parser.parseChar(headers2[i]);
-    
-}
+    parser.setInputPosition(0);
+    parser.parseOneHeaderLine(headers2);
 
     std::cout << "map-Vlaue:" << parser.getParsedHeaders().at("set-cookie") << std::endl;
 
@@ -83,60 +67,41 @@ void testHeadersWithMultipleValues() {
 }
 
 
-// void testBufferOverflowProtection() {
-//     HeaderFieldStateMachine parser;
-//     std::string header = "X-Custom-Header: " + std::string(9000, 'x') + "\r\n";
-//     bool caught = false;
-
-//     try {
-//         for (size_t i = 0; i < header.length(); ++i) {
-//             parser.parseChar(header[i]);
-//         }
-//     } catch (const std::length_error& e) {
-//         caught = true;
-//     }
-
-//     assert(caught);
-//     std::cout << "Test Buffer Overflow Protection: Passed\n";
-// }
-
-
-void testUnexpectedCharactersAfterHeaderLine() {
+void testBufferOverflowProtection() {
     HeaderFieldStateMachine parser;
-    std::string header = "Content-Type: text/html\r\n\r\n";
+    std::string header = "X-Custom-Header: " + std::string(9000, 'x') + "\r\n";
     bool caught = false;
 
-    try {
-        for (size_t i = 0; i < header.length(); ++i) {
-            parser.parseChar(header[i]);
-            if (i > strlen("Content-Type: text/html\r\n")) {
-                // Simulate unexpected characters after the header line
-                parser.parseChar('x');
-            }
-        }
-    } catch (const std::runtime_error& e) {
+
+    if (parser.parseOneHeaderLine(header) != OK)
         caught = true;
-    }
+
+    assert(caught);
+    std::cout << "Test Buffer Overflow Protection: Passed\n";
+}
+
+
+//implement logic to avoid empty header name
+void testUnexpectedCharactersAfterHeaderLine() { 
+    HeaderFieldStateMachine parser;
+    std::string header = "Content-Type: text/html\r\nx\r\n";
+    bool caught = false;
+
+    if (parser.parseOneHeaderLine(header) != OK)
+        caught = true;
 
     assert(caught);
     std::cout << "Test Unexpected Characters after Header Line: Passed\n";
 }
 
-
-
-
-
-
-
-
 // Additional test cases would be similarly structured...
 
 int main() {
-    // testValidHeader();
-    // testInvalidHeaderName();
-    // testLeadingAndTrailingWhitespace();
-    testHeadersWithMultipleValues();
-    // testBufferOverflowProtection();
+    testValidHeader();
+    testInvalidHeaderName();
+    testLeadingAndTrailingWhitespace();
+    // testHeadersWithMultipleValues();
+    testBufferOverflowProtection();
     // testUnexpectedCharactersAfterHeaderLine();
 
     // ... call other test functions
