0cab6c33d5fe1d01a64be70d88ece5d73ecaa575
Carlos Menke <carlos.menke@icloud.com>
2023-11-16 23:39:02 +0000

    added request line parsing to the state machine

--- webserv/src/server/transition.cpp
+++ webserv/src/server/transition.cpp
@@ -31,6 +31,109 @@ void trimTrailingOWS(std::string& str)
 	}
 }
 
+/*-----------------------header request line parsing----------------------*/
+
+bool isAllowedMethod(const std::string& method)
+{
+	return (method == "GET" || method == "POST" || method == "DELETE");
+}
+
+void	HeaderFieldStateMachine::handleStateHeaderMethod(char c)
+{
+	if (c == ' ' && !headerMethod.empty() && isAllowedMethod(headerMethod))
+	{
+		paramterLength = 0;
+		stateTransition(HEADER_METHOD, HEADER_URI);
+	}
+	else if (c == ' ' && headerMethod.empty())
+	{
+		throw std::runtime_error("Empty header method");
+	}
+	else if (paramterLength < HEADER_METHOD_MAX_LENGTH)
+	{
+		paramterLength++;
+		headerMethod.push_back(c);
+	}
+	else if (paramterLength > HEADER_METHOD_MAX_LENGTH)
+	{
+		throw std::length_error("Header method is too long");
+	}
+	else
+	{
+		throw std::runtime_error("Header Method not supported");
+	}
+}
+
+void	HeaderFieldStateMachine::handleStateHeaderUri(char c)
+{
+	if (c == '\r' && lastChar != '\r')
+	{
+		// Do nothing, expect line feed next
+	}
+	else if (c == '\n' && lastChar == '\r')
+	{
+		throw std::runtime_error("Wrong HTTP Version");
+	}
+	if (c == ' ' && !headerUri.empty())
+	{
+		paramterLength = 0;
+		stateTransition(HEADER_URI, HEADER_HTTP_VERSION);
+	}
+	else if (c == ' ' && headerUri.empty())
+	{
+		throw std::runtime_error("Empty header uri");
+	}
+	else if (paramterLength < HEADER_URI_MAX_LENGTH)
+	{
+		paramterLength++;
+		headerUri.push_back(c);
+	}
+	else if (paramterLength > HEADER_URI_MAX_LENGTH)
+	{
+		throw std::length_error("Header uri is too long");
+	}
+	else
+	{
+		throw std::runtime_error("Invalid character in header uri");
+	}
+}
+
+bool	isHttpVersionCorrect(const std::string& httpVersion)
+{
+	return (httpVersion == HEADER_HTTP_VERSION_STRING);
+}
+
+void	HeaderFieldStateMachine::handleStateHeaderHttpVersion(char c)
+{
+	if (c == '\r' && lastChar != '\r')
+	{
+		// Do nothing, expect line feed next
+	}
+	else if (c == '\n' && lastChar == '\r' && isHttpVersionCorrect(headerValue))
+	{
+		// End of line, transition states
+		isHttpVersionRight = true;
+		paramterLength = 0;
+		headerValue.clear();
+		stateTransition(HEADER_HTTP_VERSION, HEADER_NAME);
+	}
+	else if (c == '\n' && lastChar == '\r')
+	{
+		throw std::runtime_error("Wrong header http version");
+	}
+	else if (paramterLength < HEADER_HTTP_VERSION_MAX_LENGTH)
+	{
+		paramterLength++;
+		headerValue.push_back(c);
+	}
+	else
+	{
+		throw std::runtime_error("Wrong header http version");
+	}
+}
+
+/*---------------------header fields parsing----------------*/
+
 void	HeaderFieldStateMachine::handleStateHeaderName(char c)
 {
 	if (c == '\r' && lastChar != '\r' && headerName.empty())
@@ -55,7 +158,7 @@ void	HeaderFieldStateMachine::handleStateHeaderName(char c)
 		paramterLength++;
 		headerName.push_back(std::tolower(c));
 	}
-	else if (paramterLength >= maxHeaderLength)
+	else if (paramterLength > maxHeaderLength)
 	{
 		throw std::length_error("Header field is too long");
 	}
@@ -106,6 +209,7 @@ void	HeaderFieldStateMachine::handleStateHeaderValue(char c)
 	{
 		// Do nothing, expect line feed next
 	}
+	//TODO: Empty header for HOST is allowed
 	else if (c == '\n' && lastChar == '\r' && !headerValue.empty()) //TODO: handle duplicate header fields
 	{
 		// End of line, transition states
@@ -130,7 +234,7 @@ void	HeaderFieldStateMachine::handleStateHeaderValue(char c)
 		paramterLength++;
 		headerValue.push_back(c);
 	}
-	else if (paramterLength >= maxHeaderLength)
+	else if (paramterLength > maxHeaderLength)
 	{
 		throw std::length_error("Header field is too long");
 	}
@@ -156,6 +260,8 @@ void	HeaderFieldStateMachine::handleStateHeaderPairDone(char c)
 		throw std::runtime_error("Invalid character after header pair");
 	}
 }
+
+/*---------------------header Body parsing----------------*/
 
 void	HeaderFieldStateMachine::handleStateHeaderBody(char c)
 {
